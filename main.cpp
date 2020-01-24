b56629a6e6e2c8fbaa5fef30630b07094ccc894d
Graph-Donte-Crypto <Dimasik841@gmail.com>
2020-01-24 20:16:28 +0300

    Main commit & get all process by pid

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
-#include "mainwindow.h"
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/types.h> // for opendir(), readdir(), closedir()
+#include <sys/stat.h> // for stat()
+
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <fstream>
 
 #include <QApplication>
+#include <QTextEdit>
 
-int main(int argc, char *argv[])
-{
+#include "mainwindow.h"
+
+
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     MainWindow w;
+    w.getAllTasks();
+
     w.show();
     return a.exec();
 }
