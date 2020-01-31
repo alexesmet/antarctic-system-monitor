29cab6aaf2aa22f4359bb5d103e27c333f1a9f6a
Graph-Donte-Crypto <Dimasik841@gmail.com>
2020-01-31 17:06:24 +0300

    in Process add 2 struct for recieving CPU/stack/page faults/IPC

--- backend/proc.hpp
+++ backend/proc.hpp
@@ -6,6 +6,9 @@
 #include <cstdint>
 #include <string>
 #include <vector>
+#include <sys/times.h>
+#include <sys/resource.h>
+
 #include "utils.hpp"
 
 namespace proc {
@@ -18,6 +21,30 @@ namespace proc {
         std::string path;
         std::string name;
         std::string cmdline;
+
+        tms    cpu_time;
+        rusage res_usage;
+        /*
+        struct rusage {
+            struct timeval ru_utime; // user CPU time used
+            struct timeval ru_stime; // system CPU time used
+            long   ru_maxrss;        // maximum resident set size
+            long   ru_ixrss;         // integral shared memory size
+            long   ru_idrss;         // integral unshared data size
+            long   ru_isrss;         // integral unshared stack size
+            long   ru_minflt;        // page reclaims (soft page faults)
+            long   ru_majflt;        // page faults (hard page faults)
+            long   ru_nswap;         // swaps
+            long   ru_inblock;       // block input operations
+            long   ru_oublock;       // block output operations
+            long   ru_msgsnd;        // IPC messages sent
+            long   ru_msgrcv;        // IPC messages received
+            long   ru_nsignals;      // signals received
+            long   ru_nvcsw;         // voluntary context switches
+            long   ru_nivcsw;        // involuntary context switches
+        };
+        */
+
 
         std::string stdin;
         std::string stdout;
